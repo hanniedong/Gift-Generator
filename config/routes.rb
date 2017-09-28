6075a69688ae6a786541811391b8d8dfe5f7d1b4
Hannie Dong <hdong92@gmail.com>
2017-09-28 14:42:52 -0700

    Create sessions form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resources: :users, only: [:index, :new, :create, :show]
-  resources: :gifts, only: [:index, :new, :create, :show]
+  resources :users, only: [:index, :new, :create, :show]
+  resources :gifts, only: [:index, :new, :create, :show]
 
   get '/login ' => 'sessions#login'
   post '/login' => 'sessions#create'
