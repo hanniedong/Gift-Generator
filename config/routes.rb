cfed194326706f95ee6314132f330bd670b167fa
Hannie Dong <hdong92@gmail.com>
2017-09-28 14:35:14 -0700

    Add boostrap to project.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  resources: :users, only: [:index, :new, :create, :show]
+  resources: :gifts, only: [:index, :new, :create, :show]
+
+  get '/login ' => 'sessions#login'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#logout'
+
+  root to: 'sessions#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
