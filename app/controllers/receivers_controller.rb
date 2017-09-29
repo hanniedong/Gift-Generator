e866a9209d9b1b948928a70d64605afa8b7dedce
Hannie Dong <hdong92@gmail.com>
2017-09-29 10:40:50 -0700

    Create receiver form

--- app/controllers/receivers_controller.rb
+++ app/controllers/receivers_controller.rb
@@ -16,7 +16,7 @@ class ReceiversController < ApplicationController
 
   def show
     @receiver = Receiver.find(params[:id])
-    if logged_in? && Gift.find_by(receiver_id: @receiver.id).giver_id == current_user.id
+    if logged_in? && Receiver.find(@receiver.id).giver_id == current_user.id
       @gifts = @receiver.gifts
     else
       redirect_to root_path
